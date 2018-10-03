8c2818933148aa741191430eba1c6b17c8818439
Lynn Sokolova <ele@sprks.eu>
2018-10-03 14:58:53 +0200

    Tabs

--- spec/helpers/links.rb
+++ spec/helpers/links.rb
@@ -1,5 +1,5 @@
 module LinksHelper
-	def find_list_group_items(taxonomy_number)
-		all('.list-group')[taxonomy_number].all('.list-group-item')
-	end
+  def find_list_group_items(taxonomy_number)
+    all('.list-group')[taxonomy_number].all('.list-group-item')
+  end
 end
