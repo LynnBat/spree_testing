ff8e2a70ed270d31f6f574889f78e892e19a2b9a
Lynn Sokolova <ele@sprks.eu>
2018-10-10 23:23:52 +0200

    Corrections

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -1,4 +1,5 @@
 describe 'login_page' do
+  let(:password) { '1234567' }
   before { visit '/login' }
 
   context 'Visibility' do
@@ -20,34 +21,48 @@ describe 'login_page' do
     it 'can Log in as a User' do
       fill_in 'spree_user_email', with: ENV['USERNAME_SPREE']
       fill_in 'spree_user_password', with: ENV['PASSWORD_SPREE']
+
       find_button('Login').click
+
       expect(page).to have_css('a', text: 'Logout')
-      expect(find('.alert-success').text).to eql 'Logged in successfully'
+      alert_text = find('.alert-success').text
+      expect(alert_text).to eq 'Logged in successfully'
     end
 
     it 'cannot Log in as a User with incorrect email' do
       fill_in 'spree_user_email', with: '12345@qwe.co'
-      fill_in 'spree_user_password', with: '12345'
+      fill_in 'spree_user_password', with: password
+
       find_button('Login').click
-      expect(find('.alert-error').text).to eql 'Invalid email or password.'
+
+      alert_text = find('.alert-error').text
+      expect(alert_text).to eq 'Invalid email or password.'
     end
 
     it 'cannot Log in as a User with incorrct password' do
       fill_in 'spree_user_email', with: ENV['USERNAME_SPREE']
-      fill_in 'spree_user_password', with: '12345'
+      fill_in 'spree_user_password', with: password
+
       find_button('Login').click
-      expect(find('.alert-error').text).to eql 'Invalid email or password.'
+
+      alert_text = find('.alert-error').text
+      expect(alert_text).to eq 'Invalid email or password.'
     end
 
     it 'remembers User' do
       fill_in 'spree_user_email', with: ENV['USERNAME_SPREE']
       fill_in 'spree_user_password', with: ENV['PASSWORD_SPREE']
       check 'Remember me'
+
       find_button('Login').click
-      expect(find('.alert-success').text).to eql 'Logged in successfully'
+
+      alert_text = find('.alert-success').text
+      expect(alert_text).to eq 'Logged in successfully'
       expect(page).to have_css('a', text: 'Logout')
+
       expire_cookies
       refresh
+
       expect(page).not_to have_css('.alert-success', wait: false)
       expect(page).to have_css('a', text: 'Logout')
     end
@@ -55,11 +70,16 @@ describe 'login_page' do
     it 'doesnt remeber User' do
       fill_in 'spree_user_email', with: ENV['USERNAME_SPREE']
       fill_in 'spree_user_password', with: ENV['PASSWORD_SPREE']
+
       find_button('Login').click
-      expect(find('.alert-success').text).to eql 'Logged in successfully'
+
+      alert_text = find('.alert-success').text
+      expect(alert_text).to eq 'Logged in successfully'
       expect(page).to have_css('a', text: 'Logout')
+
       expire_cookies
       refresh
+
       expect(page).not_to have_css('.alert-success', wait: false)
       expect(page).not_to have_css('a', text: 'Logout')
     end
@@ -68,31 +88,41 @@ describe 'login_page' do
   context 'Account creation' do
     it 'can Create New Account' do
       find('a', text: 'Create a new account').click
+
       fill_in 'spree_user_email', with: Faker::Internet.unique.email
-      fill_in 'spree_user_password', with: 'asd567'
-      fill_in 'spree_user_password_confirmation', with: 'asd567'
+      fill_in 'spree_user_password', with: password
+      fill_in 'spree_user_password_confirmation', with: password
+
       find_button('Create').click
-      expect(find('.alert-notice').text).to eql 'Welcome! You have signed up successfully.'
+
+      alert_text = find('.alert-notice').text
+      expect(alert_text).to eq 'Welcome! You have signed up successfully.'
     end
 
     it 'cant create New Account with existing email' do
       find('a', text: 'Create a new account').click
+
       fill_in 'spree_user_email', with: ENV['USERNAME_SPREE']
       fill_in 'spree_user_password', with: ENV['PASSWORD_SPREE']
       fill_in 'spree_user_password_confirmation', with: ENV['PASSWORD_SPREE']
+
       find_button('Create').click
-      expect(find('.alert-danger').text).to match 'Email has already been taken'
+
+      alert_text = find('.alert-danger').text
+      expect(alert_text).to match 'Email has already been taken'
     end
 
     it 'cant create New Account with not matching passwords' do
       find('a', text: 'Create a new account').click
+
       fill_in 'spree_user_email', with: Faker::Internet.unique.email
-      fill_in 'spree_user_password', with: 'asd567'
-      fill_in 'spree_user_password_confirmation', with: 'qwerty'
+      fill_in 'spree_user_password', with: password
+      fill_in 'spree_user_password_confirmation', with: ENV['PASSWORD_SPREE']
+
       find_button('Create').click
-      expect(find('.alert-danger').text).to match "Password Confirmation doesn't match Password"
+
+      alert_text = find('.alert-danger').text
+      expect(alert_text).to match "Password Confirmation doesn't match Password"
     end
   end
-
-  xit 'can Reset my Password'
 end
