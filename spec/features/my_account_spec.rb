9bfb49e8c0479523ca542a82812b19a8c5479446
Lynn Sokolova <ele@sprks.eu>
2018-10-12 21:54:08 +0200

    My account

--- spec/features/my_account_spec.rb
+++ spec/features/my_account_spec.rb
@@ -26,30 +26,55 @@ describe 'my_account_page' do
   context 'editing information' do
     it 'can change the email' do
       find('a', text: 'Edit').click
+
       fill_in 'user_email', with: email
+      fill_in 'user_password', with: ENV['PASSWORD_SPREE']
+      fill_in 'user_password_confirmation', with: ENV['PASSWORD_SPREE']
+
       find_button('Update').click
+
       expect(page).to have_css('.alert-notice', text: 'Account updated')
     end
 
     it 'can change password' do
-      find('a', text: 'Edit').click
-      fill_in 'user_password', with: password
-      fill_in 'user_password_confirmation', with: password
-      find_button('Update').click
-      expect(page).to have_css('.alert-notice', text: 'Account updated')
+      fill_in 'spree_user_email', with: email
+      fill_in 'spree_user_password', with: ENV['PASSWORD_SPREE']
 
-      fill_in 'spree_user_email', with: ENV['USERNAME_SPREE'] #it's supposed to be 'email'
-      fill_in 'spree_user_password', with: password
       find_button('Login').click
       find('a', text: 'Edit').click
 
-      fill_in 'user_email', with: ENV['USERNAME_SPREE']
-      fill_in 'user_password', with: ENV['PASSWORD_SPREE']
-      fill_in 'user_password_confirmation', with: ENV['PASSWORD_SPREE']
+      fill_in 'user_password', with: password
+      fill_in 'user_password_confirmation', with: password
+
       find_button('Update').click
+
       expect(page).to have_css('.alert-notice', text: 'Account updated')
     end
 
-    it 'cant change password'
+    it 'cant change password' do
+      fill_in 'spree_user_email', with: email
+      fill_in 'spree_user_password', with: password
+
+      find_button('Login').click
+      find('a', text: 'Edit').click
+
+      fill_in 'user_password', with: ENV['PASSWORD_SPREE']
+      fill_in 'user_password_confirmation', with: password
+
+      find_button('Update').click
+
+      alert_text = find('.alert-danger').text
+      expect(alert_text).to match "Password Confirmation doesn't match Password"
+    end
+
+    it 'restore email and password to default' do
+      fill_in 'user_email', with: ENV['USERNAME_SPREE']
+      fill_in 'user_password', with: ENV['PASSWORD_SPREE']
+      fill_in 'user_password_confirmation', with: ENV['PASSWORD_SPREE']
+
+      find_button('Update').click
+
+      expect(page).to have_css('.alert-notice', text: 'Account updated')
+    end
   end
 end
