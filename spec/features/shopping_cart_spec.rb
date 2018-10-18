3ae33b0188c19f2b8fb74af16a50b475b63d9290
Lynn Sokolova <ele@sprks.eu>
2018-10-18 16:13:46 +0200

    the begining of work

--- spec/features/shopping_cart_spec.rb
+++ spec/features/shopping_cart_spec.rb
@@ -4,23 +4,46 @@ describe 'shopping_cart' do
   context 'empty cart' do
     before { visit '/cart' }
 
-    xit 'header displays Empty'
-    xit 'displays the title of the page'
-    xit 'your cart is empty'
-    xit 'has Continue Shopping button'
+    it 'header displays Empty' do
+      expect(page).to have_css('a', text: 'Empty')
+    end
+
+    it 'displays the title of the page' do
+      expect(page).to have_css('.col-sm-12', text: 'Shopping Cart')
+    end
+
+    it 'your cart is empty' do
+      expect(page).to have_css('.alert-info', text: 'Your cart is empty')
+    end
+
+    it 'has Continue Shopping button' do
+      expect(page).to have_css('.btn-default')
+    end
   end
 
   context 'displaying info when there are products in the cart' do
     before do
-      visit '/ruby-on-rails-bag'
+      visit '/products/ruby-on-rails-bag'
       click_button 'Add To Cart'
     end
 
     xit 'header displays number of items in the cart'
     xit 'header displays the total sum'
-    xit 'displays titles of the table'
-    xit 'the link with product name'
-    xit 'has product description'
+
+    it 'displays titles of the table' do
+      expect(page).to have_css('.cart-item-description-header', text: 'Item')
+      expect(page).to have_css('.cart-item-price-header', text: 'Price')
+      expect(page).to have_css('.cart-item-quantity-header', text: 'Qty')
+      expect(page).to have_css('.cart-item-total-header', text: 'Total')
+    end
+
+    it 'the link with product name' do
+      expect(page).to have_css('a', text: 'Ruby on Rails Bag')
+    end
+
+    it 'has product description' do
+      byebug
+    end
     xit 'has Checkout button'
     xit 'has Continue Shopping button'
   end
