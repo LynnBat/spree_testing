61b42ca1a4d64e5087fab7a2f819c26aaca3982b
Lynn Sokolova <ele@sprks.eu>
2018-10-21 23:55:20 +0200

    wrote several tests

--- spec/features/shopping_cart_spec.rb
+++ spec/features/shopping_cart_spec.rb
@@ -27,8 +27,28 @@ describe 'shopping_cart' do
       click_button 'Add To Cart'
     end
 
-    xit 'header displays number of items in the cart'
-    xit 'header displays the total sum'
+    xit 'header displays number of items in the cart' do
+      fill_in 'order_line_items_attributes_0_quantity', with: rand(2...10)
+
+      click_button 'update-button'
+
+      quantity = find('#order_line_items_attributes_0_quantity').value.to_i
+      # have to find the number, same gsub as below
+      header_items = 
+
+      expect(quantity).to eql header_items
+    end
+
+    it 'header displays the total sum' do
+      fill_in 'order_line_items_attributes_0_quantity', with: rand(2...10)
+      click_button 'update-button'
+
+      quantity = find('#order_line_items_attributes_0_quantity').value.to_i
+      total = find('.cart-total').text.gsub(/[^\d\.]/, '').to_f
+      header_total = find('.amount').text.gsub(/[^\d\.]/, '').to_f
+
+      expect(total).to eql header_total
+    end
 
     it 'displays titles of the table' do
       expect(page).to have_css('.cart-item-description-header', text: 'Item')
@@ -42,9 +62,9 @@ describe 'shopping_cart' do
     end
 
     it 'has product description' do
-      byebug
-      #expect('.line-item-description').to match "Odit vel esse voluptatibus est vero repellat"
+      expect(page).to have_css('span.line-item-description', text: 'Odit vel esse voluptatibus est vero repellat')
     end
+
     it 'has Checkout button' do
       expect(page).to have_css('#checkout-link')
     end
@@ -61,38 +81,47 @@ describe 'shopping_cart' do
     end
 
     xit 'can change the quantity' do
-      fill_in 'order_line_items_attributes_0_quantity', with: rand(1...10)
+      fill_in 'order_line_items_attributes_0_quantity', with: rand(2...10)
       click_button 'update-button'
-      #total = cart_item_price.to_i * .line_item_quantity.to_i
-      #expect(cart_item_total).to eq total
+
+      quantity = find('#order_line_items_attributes_0_quantity').value.to_i
+      # I need to fix gsub here
+      number_of_items = find('.cart-subtotal').text.gsub(/[^\d{2}\.\d{2}]/, '')
+
+      expect(quantity).to egl number_of_items
     end
 
     it 'can add more items' do
       visit '/products/ruby-on-rails-tote'
       click_button 'Add To Cart'
+
       expect(page).to have_css('a', text: 'Ruby on Rails Bag')
       expect(page).to have_css('a', text: 'Ruby on Rails Tote')
     end
 
     it 'can delete the item' do
       find('.delete').click
+
       expect(page).to have_css('.alert-info', text: 'Your cart is empty')
     end
 
     it 'item deletes automatically after setting the quantity to 0' do
       fill_in 'order_line_items_attributes_0_quantity', with: 0
       click_button 'update-button'
+
       expect(page).to have_css('.alert-info', text: 'Your cart is empty')
     end
 
     it 'can Empty the cart' do
       click_button 'Empty Cart'
+
       expect(page).to have_css('.alert-info', text: 'Your cart is empty')
     end
 
     it 'can add the GWP Code' do
       fill_in 'order_coupon_code', with: 'gwp'
       click_button 'Apply'
+
       expect(page).to have_css('a', text: 'Spree Mug')
     end
   end
@@ -103,15 +132,37 @@ describe 'shopping_cart' do
       click_button 'Add To Cart'
     end
 
-    xit 'header changes number of products'
-    xit 'header changes the price'
-    xit 'Total for each Item Calculations'
-    xit 'Total for the whole order'
+    it 'Total for each Item Calculations' do
+      fill_in 'order_line_items_attributes_0_quantity', with: rand(2...10)
+      click_button 'update-button'
+
+      one_item_price = find('.cart-item-price').text.gsub(/[^\d\.]/, '').to_f
+      quantity = find('#order_line_items_attributes_0_quantity').value.to_i
+      total_calculation = one_item_price * quantity
+      total_cart = find('.cart-item-total').text.gsub(/[^\d\.]/, '').to_f
+
+      expect(total_cart).to eq total_calculation.round(2)
+    end
+
+    xit 'Total for the whole order' do
+      visit '/products/ruby-on-rails-tote'
+      click_button 'Add To Cart'
+
+      # find all and add all of them
+      find_all('.cart-item-total')[taxonomy_number]
+    end
 
     xit 'add discount code and see calculations' do
       fill_in 'order_coupon_code', with: 'segment'
       click_button 'Apply'
-      expect(page).to have_css('.adjustment', text: "Adjustment: Promotion (Discount)")
+
+      # I need to fix gsub here
+      subtotal = find('.cart-subtotal').text.gsub(/[^\d{2}\.\d{2}]/, '')
+      promotion = subtotal / 2
+      to_pay = subtotal - promotion.round(2)
+      total = find('.cart-total').text.gsub(/[^\d\.]/, '').to_f
+
+      expect(total).to eql to_pay
     end
   end
 end
