908ca278d6029cd909b0637bd16afec6e687c6f9
Lynn Sokolova <ele@sprks.eu>
2018-12-18 17:01:28 +0100

    only pictures have left

--- spec/features/pdp_spec.rb
+++ spec/features/pdp_spec.rb
@@ -1,72 +1,36 @@
 describe 'pdp' do
   let(:router) { Router.new }
 
-  before(:each) do
-    admin_login
+  context 'All elements are present' do
+    before do
+      admin_login
 
-    visit router.admin_item_path
-
-    @title = find('#product_name').value
-    @description = find('#product_description').value
-    @price = find('#product_price').value
-
-    visit router.admin_item_variants_path
-    @variants = []
-    table = find('.ui-sortable').all('tr')
-    table.each do |variant|
-      within(variant) { @variants << all('td')[1].text }
-    end
-
-    visit router.admin_item_properties_path
-    @properties = all('.form-control').collect(&:value).reject(&:empty?)
-
-    Capybara.reset_session!
-  end
-
-  before { visit router.pdp_path }
-
-  context 'Breadcrumbs' do
-    it 'are present' do
-      expect(page).to have_css('.breadcrumb')
-    end
-
-    xit 'redirect to the right page' do
-      title = find('.breadcrumb').all('a')
-      title[0].click
-      expect(page.current_url).to eq "#{Capybara.app_host}/"
-      page.driver.go_back
-
-      title = find('.breadcrumb').all('a')
-      link = title[1].text.downcase
-      title[1].click
-      expect(page.current_url).to eq "#{Capybara.app_host}/#{link}"
-      page.driver.go_back
-
+      visit router.admin_item_path
       byebug
 
-      title = find('.breadcrumb').all('a')[2..-1]
-      link = title.text.downcase
-      title.click
-      expect(page.current_url).to eq "#{Capybara.app_host}/t/#{link}"
-      page.driver.go_back
-    end
-  end
+      @title = find('#product_name').value
+      @description = find('#product_description').value
+      @price = find('#product_price').value
 
-  context 'Pictures' do
-    it 'are present' do
-      byebug
-      # expect(page).to have_css("img[src*='ror_baseball_jersey_red.png']")
-      expect(page).to have_css('.thumbnails.list-inline')
+      visit admin_item_variants_path
+      @pictures = find('.table.sortable').all('img').collect { |img| img[:src].split('/').last }
+
+      visit router.admin_item_variants_path
+      @variants = []
+      table = find('.ui-sortable').all('tr')
+      table.each do |variant|
+        within(variant) { @variants << all('td')[1].text }
+      end
+
+      visit router.admin_item_properties_path
+      @properties = all('.form-control').collect(&:value).reject(&:empty?)
+
+      Capybara.reset_session!
+
+      visit router.pdp_path
     end
 
-    xit 'can choose different picture'
-    xit 'while hovering the picture, main is changed' do
-      # all('img.thumbnail').last.hover
-    end
-  end
-
-  context 'properties+description' do
-    it 'all info is displayed' do
+    it 'info' do
       pdp_title = find('.product-title').text
       pdp_description = find('.well').text
       pdp_price = find('.lead.price.selling').text
@@ -74,13 +38,48 @@ describe 'pdp' do
       pdp_properties = find('#product-properties').all('td').collect(&:text)
 
       aggregate_failures do
+        expect(page).to have_css('.breadcrumb')
         expect(pdp_title).to eq @title
         expect(pdp_description).to eq @description
         expect(pdp_price).to include @price
         expect(pdp_variants).to eq @variants
         expect(pdp_properties).to eq @properties
         expect(page).to have_css('.product-section-title', text: 'Properties')
+        expect(page).to have_css('.product-section-title', text: 'Variants')
+        expect(page).to have_css('.product-section-title', text: 'Look for similar items')
       end
+    end
+
+    it 'pictures' do
+      byebug
+      pdp_pictures = find('.thumbnails.list-inline').all('img')[0..1].collect { |img| img[:src].split('/').last }
+      # expect(page).to have_css("img[src*='ror_baseball_jersey_red.png']")
+      # @pictures[2..-1].each
+      expect(pdp_pictures).to eq @pictures
+    end
+  end
+
+  context 'Functionality' do
+    before { visit router.pdp_path }
+
+    it 'breadcrumbs redirect to the right page' do
+      breadcrumbs = find('.breadcrumb').all('a').collect(&:text)
+      breadcrumbs.each do |breadcrumb|
+        find('.breadcrumb').click_link breadcrumb
+
+        if breadcrumb == 'Home'
+          expect(page.current_url).to eq "#{Capybara.app_host}/"
+        else
+          expect(page.current_url).to include breadcrumb.downcase
+        end
+
+        go_back
+      end
+    end
+
+    xit 'can choose different picture'
+    xit 'while hovering the picture, main is changed' do
+      # all('img.thumbnail').last.hover
     end
 
     it 'can choose any variant' do
@@ -98,19 +97,22 @@ describe 'pdp' do
         visit router.pdp_path
       end
     end
-  end
 
-  context 'description block' do
-    xit 'can change the quantity: to write it, to use the arrows'
-    xit 'can add to the card'
-  end
+    it 'can change the quantity' do
+      quantity_pdp = rand(1..10)
+      fill_in 'quantity', with: quantity_pdp
+      click_button 'Add To Cart'
 
-  context 'similar items' do
-    it 'can see the title of the block' do
-      expect(page).to have_css('.product-section-title', text: 'Look for similar items')
+      quantity_cart = find('.line_item_quantity').value.to_i
+      expect(quantity_pdp).to eq quantity_cart
     end
 
-    it 'can be redirected to those items' do
+    it 'can add to the card' do
+      click_button 'Add To Cart'
+      expect(page.current_url).to include 'cart'
+    end
+
+    it 'can be redirected to similar items' do
       names = all('.list-group-item').collect(&:text)
       names.each do |item|
         within('#taxon-crumbs') { click_link item }
@@ -118,5 +120,5 @@ describe 'pdp' do
         go_back
       end
     end
-  end
+  end  
 end
---
+++ spec/support/router.rb
@@ -0,0 +1,21 @@
+class Router
+  def pdp_path
+    '/products/ruby-on-rails-baseball-jersey'
+  end
+
+  def admin_item_path
+    '/admin/products/ruby-on-rails-baseball-jersey/edit'
+  end
+
+  def admin_item_pictures_path
+    'admin/products/ruby-on-rails-baseball-jersey/images'
+  end
+
+  def admin_item_variants_path
+    '/admin/products/ruby-on-rails-baseball-jersey/variants'
+  end
+
+  def admin_item_properties_path
+    '/admin/products/ruby-on-rails-baseball-jersey/product_properties'
+  end
+end
